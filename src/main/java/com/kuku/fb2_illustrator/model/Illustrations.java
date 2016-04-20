7668999fff837ccd42233169cbd8404900951506
maxifly <maxpant@mail.ru>
2016-04-21 00:48:25 +0300

    chine Illustrations

--- src/main/java/com/kuku/fb2_illustrator/model/Illustrations.java
+++ src/main/java/com/kuku/fb2_illustrator/model/Illustrations.java
@@ -21,8 +21,8 @@ public class Illustrations {
             .getLocLogger(BookParse.class.getName());
 
     private Vector<Illustration> allIllustrations = new Vector<>();
-    private Map<PType, Illustration> illustratedParagrafs = new HashMap<>();
-    private Map<Illustration, PType > chineIllustrations   = new HashMap<>();
+    private Map<PType, Set<Illustration>> illustratedParagrafs = new HashMap<>();
+    private Map<Illustration, PType> chineIllustrations = new HashMap<>();
 
     private Set<Integer> chinedIllustrationIndexes = new TreeSet<>();
     private int lastChainIllustration = 0;
@@ -40,8 +40,19 @@ public class Illustrations {
      */
     public void illustratedParagraf(PType paragraf, Illustration illustration) {
         int illIndex = this.allIllustrations.indexOf(illustration);
-        illustratedParagrafs.put(paragraf, illustration);
-        chineIllustrations.put(illustration,paragraf);
+
+        Set<Illustration> illSet = illustratedParagrafs.get(paragraf);
+        if (illSet == null) {
+            illustratedParagrafs.put(paragraf, new LinkedHashSet<Illustration>());
+        }
+
+        if (illSet.add(illustration)) {
+            log.debug("Chine illustration " + illustration + " with paragraf.");
+        } else {
+            log.warn("Illustration " + illustration + " already chined with paragraf.");
+        }
+
+        chineIllustrations.put(illustration, paragraf);
         this.chinedIllustrationIndexes.add(illIndex);
         if (illIndex > lastChainIllustration) this.lastChainIllustration = illIndex;
 
@@ -63,59 +74,60 @@ public class Illustrations {
 
     /**
      * расставляет несвязанные иллюстрации просто по номерам параграфов
+     *
      * @param paragrafs - параграфы
      */
     public void chineByOrder(Paragrafs paragrafs) {
         Iterator<Integer> chined = chinedIllustrationIndexes.iterator();
         Integer startIndex = 0, endIndex = 0;
-        Map<Integer,Integer> ill_par_chain = new HashMap<>();
+        Map<Integer, Integer> ill_par_chain = new HashMap<>();
 
         while (chined.hasNext()) {
             // Переходим к следующему окну
             startIndex = endIndex;
             endIndex = chined.next();
-            addNotChined(startIndex,endIndex,paragrafs,ill_par_chain);
+            addNotChined(startIndex, endIndex, paragrafs, ill_par_chain);
         }
 
 
-        //TODO Надо обработать ситуацию, когда остался последнее окно/ И еще надо определить  это последнее окно
         startIndex = endIndex;
-        endIndex = allIllustrations.size()-1;
-
+        endIndex = allIllustrations.size() - 1;
+        addNotChined(startIndex, endIndex, paragrafs, ill_par_chain);
     }
 
 
     public void addNotChined(Integer startIndex,
                              Integer endIndex,
                              Paragrafs paragrafs,
-                             Map<Integer,Integer> ill_par_chain) {
+                             Map<Integer, Integer> ill_par_chain) {
 
         Integer startParagraf, endParagraf;
 
-        if (endIndex > startIndex + 1 ) {
+        if (endIndex > startIndex + 1) {
             // Есть промежуток
             // Определим стартовый и конечный номера параграфов
             startParagraf =
                     paragrafs.getParagrafNumber(
-                    chineIllustrations.get(allIllustrations.get(startIndex)));
+                            chineIllustrations.get(allIllustrations.get(startIndex)));
             endParagraf =
                     paragrafs.getParagrafNumber(
                             chineIllustrations.get(allIllustrations.get(endIndex)));
 
             log.debug("Start paragraf " + startParagraf + " endParagraf " + endParagraf);
 
-               int notChinedIll = endIndex-startIndex; // Сколько не связано иллюстраций
-               int step = ((endParagraf - startParagraf) / notChinedIll) + 1;
+            int notChinedIll = endIndex - startIndex; // Сколько не связано иллюстраций
+            int step = ((endParagraf - startParagraf) / notChinedIll) + 1;
 
-            log.debug("notChinedIll " + notChinedIll + " step " +step);
+            log.debug("notChinedIll " + notChinedIll + " step " + step);
 
             // теперь начинаем связывать
 
             int chinedParagraf = startParagraf;
-            for (int i = (startIndex+1) ; i < endIndex; i++ ) {
+            for (int i = (startIndex + 1); i < endIndex; i++) {
                 chinedParagraf += step;
                 if (chinedParagraf > endParagraf) chinedParagraf = endParagraf;
-                ill_par_chain.put(i,chinedParagraf);
+                ill_par_chain.put(i, chinedParagraf);
+                log.info("Not chined illustration number " + i + " chine with paragraf");
             }
 
         }
