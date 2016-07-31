5275e875f4b9cf473722ea32dff99a56ec0966ce
maxifly <maxpant@mail.ru>
2016-07-31 14:35:36 +0300

    fix

--- src/main/java/com/maxifly/fb2_illustrator/GUI/Controllers/Ctrl_CertainAlbum.java
+++ src/main/java/com/maxifly/fb2_illustrator/GUI/Controllers/Ctrl_CertainAlbum.java
@@ -3,14 +3,18 @@ package com.maxifly.fb2_illustrator.GUI.Controllers;
 import com.maxifly.fb2_illustrator.GUI.DomainModel.DM_CertainAlbum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Maximus on 31.07.2016.
  */
-public class Ctrl_CertainAlbum {
+public class Ctrl_CertainAlbum implements Initializable{
     final private DM_CertainAlbum dm_certainAlbum;
 
     @FXML private TextField book_name;
@@ -33,5 +37,16 @@ public class Ctrl_CertainAlbum {
 
     public Ctrl_CertainAlbum(DM_CertainAlbum dm_certainAlbum) {
         this.dm_certainAlbum = dm_certainAlbum;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        book_name.textProperty().bindBidirectional(dm_certainAlbum.book_name_Propery());
+        album_addr.textProperty().bindBidirectional(dm_certainAlbum.album_addr_Propery());
+        book_src_file.textProperty().bindBidirectional(dm_certainAlbum.book_src_file_Propery());
+        book_dst_file.textProperty().bindBidirectional(dm_certainAlbum.book_dst_file_Propery());
+
+
+        book_check.selectedProperty().bindBidirectional(dm_certainAlbum.book_check_Propery());
     }
 }
--- src/main/java/com/maxifly/fb2_illustrator/GUI/DomainModel/DM_CertainAlbum.java
+++ src/main/java/com/maxifly/fb2_illustrator/GUI/DomainModel/DM_CertainAlbum.java
@@ -1,7 +1,44 @@
 package com.maxifly.fb2_illustrator.GUI.DomainModel;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Created by Maximus on 31.07.2016.
  */
 public class DM_CertainAlbum {
+    private StringProperty book_name;
+
+    private StringProperty album_addr;
+    private StringProperty book_src_file;
+    private StringProperty book_dst_file;
+
+    private BooleanProperty book_check;
+
+    public DM_CertainAlbum() {
+        book_name = new SimpleStringProperty();
+        album_addr = new SimpleStringProperty();
+        book_src_file = new SimpleStringProperty();
+        book_dst_file = new SimpleStringProperty();
+        book_check = new SimpleBooleanProperty();
+    }
+
+    public StringProperty book_name_Propery() {
+        return book_name;
+    }
+    public StringProperty album_addr_Propery() {
+        return album_addr;
+    }
+    public StringProperty book_src_file_Propery() {
+        return book_src_file;
+    }
+    public StringProperty book_dst_file_Propery() {
+        return book_dst_file;
+    }
+
+    public BooleanProperty book_check_Propery() {
+        return book_check;
+    }
 }
