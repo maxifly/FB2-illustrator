5d54042a50acb831d91dc866d9aecf6f37ac8a98
maxifly <maxpant@mail.ru>
2016-08-26 01:06:10 +0300

    SearchTemplate

---
+++ src/main/java/com/maxifly/fb2_illustrator/GUI/Controllers/Ctrl_SearchTemplate.java
@@ -0,0 +1,42 @@
+package com.maxifly.fb2_illustrator.GUI.Controllers;
+
+import com.maxifly.fb2_illustrator.GUI.DomainModel.DM_SearchTemplate;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Maximus on 26.08.2016.
+ */
+public class Ctrl_SearchTemplate implements Initializable {
+
+    private DM_SearchTemplate dm_searchTemplate;
+
+    @FXML
+    TextField template;
+    @FXML
+    TextField description;
+
+    @FXML
+    RadioButton type_text;
+    @FXML
+    RadioButton type_reg;
+
+
+
+
+
+    public Ctrl_SearchTemplate(DM_SearchTemplate dm_searchTemplate) {
+        this.dm_searchTemplate = dm_searchTemplate;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/com/maxifly/fb2_illustrator/GUI/DomainModel/DM_SearchTemplate.java
@@ -0,0 +1,7 @@
+package com.maxifly.fb2_illustrator.GUI.DomainModel;
+
+/**
+ * Created by Maximus on 26.08.2016.
+ */
+public class DM_SearchTemplate {
+}
