959c18e9383253c7ba30d10cdd1424395d1a6127
Maxim.Pantuhin <Maxim.Pantuhin@billing.ru>
2016-08-25 18:46:52 +0300

    фикс

---
+++ src/main/java/com/maxifly/fb2_illustrator/model/Project.java
@@ -0,0 +1,27 @@
+package com.maxifly.fb2_illustrator.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Maxim.Pantuhin on 25.08.2016.
+ */
+public class Project {
+    private String id = "123456789";
+    private Set<SearchTemplate_POJO> bookNameTemplates = new HashSet<>();
+    private List<Illustration> illustrations = new ArrayList(); //TODO тут надо как-то сохранять пор\док вставки
+
+
+
+    public void addBookNameTempale(SearchTemplate_POJO bookNameTemplate) {
+        this.bookNameTemplates.add(bookNameTemplate);
+    }
+
+    public void addIll(Illustration illustration) {
+        this.illustrations.add(illustration);
+    }
+
+
+}
