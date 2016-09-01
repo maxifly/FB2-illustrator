46bf14231d96b3de08e16fc67233c1b0d10b1097
Maxim.Pantuhin <Maxim.Pantuhin@billing.ru>
2016-09-01 16:20:06 +0300

    selected and show ill

--- src/main/java/com/maxifly/fb2_illustrator/GUI/Controllers/Ctrl_Project.java
+++ src/main/java/com/maxifly/fb2_illustrator/GUI/Controllers/Ctrl_Project.java
@@ -1,6 +1,5 @@
 package com.maxifly.fb2_illustrator.GUI.Controllers;
 
-import com.maxifly.fb2_illustrator.GUI.DomainModel.DM_Abstract;
 import com.maxifly.fb2_illustrator.GUI.DomainModel.DM_Ill;
 import com.maxifly.fb2_illustrator.GUI.DomainModel.DM_Project;
 import com.maxifly.fb2_illustrator.GUI.Factory_GUI;
@@ -8,12 +7,18 @@ import com.maxifly.fb2_illustrator.GUI.GUI_Obj;
 import com.maxifly.fb2_illustrator.model.Illustration;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -24,11 +29,15 @@ public class Ctrl_Project extends Ctrl_Abstract implements Initializable
     @FXML
     VBox illustrations;
 
+    @FXML
+    BorderPane document_pane;
 
     private Factory_GUI factory_gui;
     private DM_Project dm_project;
 
     private ObjectProperty<DM_Ill> selected_ill = new SimpleObjectProperty<>();
+
+    private Map<DM_Ill, Node> illNodes = new HashMap<>();
 
 
     public Ctrl_Project(Factory_GUI factory_gui, DM_Project dm_project) {
@@ -36,13 +45,23 @@ public class Ctrl_Project extends Ctrl_Abstract implements Initializable
         this.dm_project = dm_project;
     }
 
+
+    private void changeSelected_Ill(ObservableValue<? extends DM_Ill> observable, DM_Ill oldValue, DM_Ill newValue) {
+        Node node = illNodes.get(newValue);
+        document_pane.setCenter(node);
+    }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        for (Illustration ill:dm_project.getIllList()) {
+        for (Illustration ill:dm_project.getIllList()) { //TODO здесь надо на биндинг поменять
             try {
                 GUI_Obj gui_obj = factory_gui.createIll(ill);
                 GUI_Obj gui_obj_ico = factory_gui.createIllIco( (DM_Ill) gui_obj.dm_model);
+
+                illNodes.put((DM_Ill) gui_obj.dm_model,gui_obj.node);
+
 
                 selected_ill.bindBidirectional( ((Ctrl_IllIco) gui_obj_ico.controll).selected_dm_ill_Property());
 
@@ -51,6 +70,13 @@ public class Ctrl_Project extends Ctrl_Abstract implements Initializable
                 e.printStackTrace();
             }
         }
+
+        selected_ill.addListener(new ChangeListener<DM_Ill>() {
+            @Override
+            public void changed(ObservableValue<? extends DM_Ill> observable, DM_Ill oldValue, DM_Ill newValue) {
+                changeSelected_Ill(observable,  oldValue,  newValue);
+            }
+        });
 
     }
 }
